3df9a1946cc7cda3dfd1b903392f1b4687ff7b46
Daniel Fugina <dan@ablecanyon.com>
2014-03-14 10:36:12 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Catalog Secure Rewrite Extension
+======================
+
+Redirects category and product page requests over ssl to their non-ssl versions.
---
+++ app/code/local/AbleCanyon/SecureRewrite/Model/Observer.php
@@ -0,0 +1,23 @@
+<?php
+
+class AbleCanyon_SecureRewrite_Model_Observer
+{
+    /**
+     * Redirect category and product page requests over ssl to their non-ssl versions.
+     *
+     * @param Varien_Event_Observer $observer
+     * @return AbleCanyon_SecureRewrite_Model_Observer $this
+     */
+    public function unsecureRewrite(Varien_Event_Observer $observer)
+    {
+        $action = $observer->getControllerAction();
+        $request = $action->getRequest();
+
+        if ($request->isSecure()) {
+            $action->getResponse()->setRedirect(Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_LINK, false) . $request->getServer('REQUEST_URI'));
+            $action->getRequest()->setDispatched(true);
+        }
+
+        return $this;
+    }
+}
